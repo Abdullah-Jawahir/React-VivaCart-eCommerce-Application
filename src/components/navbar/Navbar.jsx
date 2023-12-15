0c62b4b9b1f459994816e649215ba1837c339c80
Abdullah-Jawahir <mjabdullah33@gmail.com>
2023-12-15 22:24:10 +0530

    added navabr responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,17 +5,26 @@ import cartIcon from '../assets/cart_icon.png';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../contexts/ShopContext';
 
+
 export const Navbar = () => {
 
     const initialActiveMenu = sessionStorage.getItem('activeMenu') || '';
 
     const [activeMenu, setActiveMenu] = useState(initialActiveMenu);
+    const [isBxIconClicked, setIsBxIconClicked] = useState(false)
 
     useEffect(() => {
         sessionStorage.setItem('activeMenu', activeMenu);
-    }, [activeMenu]);
+    }, [activeMenu, isBxIconClicked]);
 
     const { cartCount } = useContext(ShopContext);
+
+    const handleBxIconClick = () => {
+
+        setIsBxIconClicked((prev) => !prev);
+        setActiveMenu("");
+    }
+
 
     return (
         <div className='navbar'>
@@ -23,11 +32,33 @@ export const Navbar = () => {
                 <Link to='/' onClick={() => { setActiveMenu("shop") }}><img src={logoBig} alt="logo" /></Link>
             </div>
 
-            <nav className='nav-list'>
-                <span onClick={() => { setActiveMenu("shop") }} className={`nav-item ${activeMenu === "shop" ? 'active' : ''}`}><Link to='/'>shop</Link></span>
-                <span onClick={() => { setActiveMenu("men") }} className={`nav-item ${activeMenu === "men" ? 'active' : ''}`}><Link to='/men'>men</Link></span>
-                <span onClick={() => { setActiveMenu("women") }} className={`nav-item ${activeMenu === "women" ? 'active' : ''}`}><Link to='/women'>women</Link></span>
-                <span onClick={() => { setActiveMenu("kids") }} className={`nav-item ${activeMenu === "kids" ? 'active' : ''}`}><Link to='/kids'>kids</Link></span>
+            <i
+                onClick={() => { handleBxIconClick() }}
+                className={`bx ${isBxIconClicked ? 'bx-x' : 'bx-menu-alt-left'}`}>
+
+            </i>
+
+            <nav className={`nav-list ${isBxIconClicked ? 'open-nav-list' : ''}`}>
+                <span
+                    onClick={() => { setActiveMenu("shop") }}
+                    className={`nav-item ${activeMenu === "shop" ? 'active' : ''} ${isBxIconClicked ? 'show' : ''}`}>
+                    <Link to='/'>shop</Link>
+                </span>
+                <span
+                    onClick={() => { setActiveMenu("men") }}
+                    className={`nav-item ${activeMenu === "men" ? 'active' : ''} ${isBxIconClicked ? 'show' : ''}`}>
+                    <Link to='/men'>men</Link>
+                </span>
+                <span
+                    onClick={() => { setActiveMenu("women") }}
+                    className={`nav-item ${activeMenu === "women" ? 'active' : ''} ${isBxIconClicked ? 'show' : ''}`}>
+                    <Link to='/women'>women</Link>
+                </span>
+                <span
+                    onClick={() => { setActiveMenu("kids") }}
+                    className={`nav-item ${activeMenu === "kids" ? 'active' : ''} ${isBxIconClicked ? 'show' : ''}`}>
+                    <Link to='/kids'>kids</Link>
+                </span>
             </nav>
 
             <div className="nav-login-cart">
